e1197a783442c297df9811e42c4e47b5c76aa202
lucas <lucasgbrsouza@gmail.com>
2019-11-10 19:25:42 -0300

    Classe dos atributos e métodos de cálculo

---
+++ ListaRegistroEmpregado/Empregados.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ListaRegistroEmpregado
+{
+    class Empregados
+    {
+        private string Nome { get; set; }
+        public int Id { get; set; }
+        private double Salario { get; set; }
+
+        public Empregados(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+
+        public void aumentoSalario(double value)
+        {
+            Salario += (Salario * (value/100));     
+        }
+
+        public override string ToString()
+        {
+            return "Id: " + Id + ", Nome: " + Nome + ", Salário: R$ " + Salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}

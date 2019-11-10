bcb8eed19fd59107ee46c7b6af7bb84dd042b409
lucas <lucasgbrsouza@gmail.com>
2019-11-10 16:49:50 -0300

    Classe de inicio

---
+++ POO/ExemploPolimorfismo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ExemploPolimorfismo.Biblioteca;
+using ExemploPolimorfismo.Biblioteca.Heranca;
+
+namespace ExemploPolimorfismo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Veiculo vei = new Veiculo();
+            Carro car = new Carro();
+            Moto mot = new Moto();
+
+            moverVeiculos(vei, car, mot);
+        }
+
+        public static void moverVeiculos(Veiculo veiculos, Carro carros, Moto motos)
+        {
+            veiculos.Mover();
+            carros.Mover();
+            motos.Mover();
+        }
+    }
+}

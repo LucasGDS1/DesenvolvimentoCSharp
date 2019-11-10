70e22feb3118be4562996406f258772307180ee9
lucas <lucasgbrsouza@gmail.com>
2019-11-10 17:53:57 -0300

    Classe de atributos sem a utilização de métodos

---
+++ DadosFuncionario/Funcionario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DadosFuncionario
+{
+    class Funcionario
+    {
+        public string nome;
+        public double salario;
+    }
+}

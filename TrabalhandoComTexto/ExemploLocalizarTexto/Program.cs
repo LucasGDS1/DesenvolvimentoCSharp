a00125f448572959e9e60594e266d6ccd19536f2
lucas <lucasgbrsouza@gmail.com>
2019-11-10 16:58:31 -0300

    Classe principal

---
+++ TrabalhandoComTexto/ExemploLocalizarTexto/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExemploLocalizarTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string textos = "Ola mundo da programação C#";
+
+            //IndexOf vai localizar a primeira palavra especificada.
+            Console.WriteLine("Posição da palavra mundo: " + textos.IndexOf("mundo"));
+            //LastIndexOf vai localicar a última palavra especificada.
+            Console.WriteLine("Posição da última palavra do texto: " + textos.LastIndexOf("C#"));
+        }
+    }
+}

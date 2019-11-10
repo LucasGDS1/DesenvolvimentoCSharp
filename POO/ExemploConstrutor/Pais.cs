912d46b148ef8896305fc40c4d9de4e7ff5eb616
lucas <lucasgbrsouza@gmail.com>
2019-11-10 16:40:35 -0300

    Exemplos de construtor

---
+++ POO/ExemploConstrutor/Pais.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExemploConstrutor
+{
+    class Pais
+    {
+        public string pais;
+        public string cidade;
+        public string estado;
+
+        public Pais()
+        {
+            pais = "Brasil";
+            cidade = "Curitiba";
+        }
+        public Pais(string pais, string estado, string cidade)
+        {
+            this.pais = pais;
+            this.cidade = cidade;
+            this.estado = estado;
+        }
+    }
+}
---
+++ POO/ExemploConstrutor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExemploConstrutor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pais localidade = new Pais();
+
+            Console.WriteLine("Pais: " + localidade.pais + " Cidade: " + localidade.cidade);
+
+            Pais locais = new Pais("Estados Unidos", "California", "Los Angeles");
+
+            Console.WriteLine("País: " + locais.pais + " Estado: " + locais.estado + " Cidade: " + locais.cidade);
+        }
+    }
+}

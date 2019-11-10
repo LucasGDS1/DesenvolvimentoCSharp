687b724f14101fb536e3d92b74330aada13baf69
lucas <lucasgbrsouza@gmail.com>
2019-11-10 17:50:45 -0300

    Classe dos atributos

---
+++ Pessoas/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pessoas
+{
+    class Pessoa
+    {
+        public string nome;
+        public int idade;
+
+
+        public override string ToString()
+        {
+            return "Pessoa mais velha: " + nome;
+        }
+    }
+}

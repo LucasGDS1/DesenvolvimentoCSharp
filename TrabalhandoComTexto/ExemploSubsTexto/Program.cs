ad20b0e4129c4127f0305e14ddf6a287161da25f
lucas <lucasgbrsouza@gmail.com>
2019-11-10 17:00:13 -0300

    Classe principal

---
+++ TrabalhandoComTexto/ExemploSubsTexto/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExemploSubsTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "ela";
+
+            Console.WriteLine("Nome da bendita: " + nome);
+
+            nome = nome.Replace("ela", "elas");
+
+            Console.WriteLine("O nome das benditas: " + nome);
+        }
+    }
+}

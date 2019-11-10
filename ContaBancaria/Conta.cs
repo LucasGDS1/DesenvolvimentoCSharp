bbdd8585f19c65ae499c15771e94859b8ec9b136
lucas <lucasgbrsouza@gmail.com>
2019-11-10 16:55:42 -0300

    Classe dos atributos

---
+++ ContaBancaria/Conta.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ContaBancaria
+{
+    class Conta
+    {
+        private string titular { get; set; }
+        private int numeroConta;
+        private double saldo;
+        private double taxaDeSaque = 5.00;
+        
+        public Conta(string titular, int numeroConta)
+        {
+            this.titular = titular;
+            this.numeroConta = numeroConta;
+        }
+
+        public void deposito(double value)
+        {
+            saldo += value;
+        }
+
+        public void saque(double value)
+        {
+            saldo -= value;
+            saldo -= taxaDeSaque;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + numeroConta + ", Titular: " + titular + ", Saldo: R$ " + saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+
+    }
+}

ce0ccab9735e925bdc3b8ee75561bd8e532baf17
lucas <lucasgbrsouza@gmail.com>
2019-11-10 16:51:51 -0300

    Classe de inicio

---
+++ POO/ExemploSobescrita/Program.cs
@@ -0,0 +1,19 @@
+﻿using ExemploSobescrita.Veiculo;
+using System;
+
+namespace ExemploSobescrita
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro car = new Carro();
+            Moto mot = new Moto();
+            Application app = new Application();
+
+            car.Mover();
+            mot.Mover();
+            app.Mover();
+        }
+    }
+}

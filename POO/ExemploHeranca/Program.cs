9b059f1a51054ffada22b5541c0fdefdecdc0a8c
lucas <lucasgbrsouza@gmail.com>
2019-11-10 16:43:52 -0300

    Classe de inicio

---
+++ POO/ExemploHeranca/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ExemploHeranca.Veiculo;
+
+namespace ExemploHeranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro car = new Carro();
+            Moto mot = new Moto();
+
+            car.anoFabricacao = 2019;
+            mot.marca = "BMW";
+
+            Console.WriteLine(mot.marca);
+        }
+    }
+}

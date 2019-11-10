ca6c449c0a3cd104dcb57fc8d94bc83a579b8654
lucas <lucasgbrsouza@gmail.com>
2019-11-10 16:48:39 -0300

    Exemplo de Polimorfismo

---
+++ POO/ExemploPolimorfismo/Biblioteca/Veiculo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExemploPolimorfismo.Biblioteca
+{
+    class Veiculo
+    {
+
+        public virtual void Mover()
+        {
+            Console.WriteLine("O veiculo se moveu!");
+        }
+    }
+}

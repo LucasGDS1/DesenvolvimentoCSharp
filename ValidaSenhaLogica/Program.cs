c80ed97a8ce7fe401bdea3cfda5b20bcfb008bbd
lucas <lucasgbrsouza@gmail.com>
2019-11-10 17:08:55 -0300

    Classe principal

---
+++ ValidaSenhaLogica/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ValidaSenhaLogica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = "2002";
+
+            Console.WriteLine("Dígite sua senha");
+            string lerSenha = Console.ReadLine();
+
+            //Caso o usuário dígite a senha certa logo de primeira, entra no IF e Printa que o acesso foi permitido
+            //Caso contrário, entra no While e fica solicitando ao usuário que coloque a senha novamente
+            if (lerSenha == senha)
+            {
+                Console.WriteLine("\nAcesso permitido");
+            }
+
+            while (lerSenha != senha)
+            {
+                
+                Console.WriteLine("\nSenha invalida");
+                Console.WriteLine("\nTente novamente");
+                lerSenha = Console.ReadLine();
+
+                if(lerSenha == senha)
+                {
+                    Console.WriteLine("\nAcesso permitido");
+                } 
+            }
+        }
+    }
+}

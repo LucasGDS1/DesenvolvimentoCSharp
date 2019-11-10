977e63d632a7c02490b1e7ca25e6f6e4eb35b095
lucas <lucasgbrsouza@gmail.com>
2019-11-10 17:14:04 -0300

    Classe principal

---
+++ EntradaeSaida/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EntradaeSaida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int CodigoASCII = Console.Read();
+            char letra = (char)CodigoASCII;
+            Console.WriteLine("Código ASCII: " + CodigoASCII + " Letra digitada: " + letra);
+
+            //ConsoleKeyInfo console = Console.ReadKey();
+        }
+    }
+}

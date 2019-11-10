e0dbb8ba9459f8a31155735cdbc67f326282c862
lucas <lucasgbrsouza@gmail.com>
2019-11-10 16:57:51 -0300

    Classe principal

---
+++ TrabalhandoComTexto/ExemploFormatarTexto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ExemploFormatarTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "lucas";
+
+            string textoFormatado = string.Format("Bem vindo a programação {0}!", nome);
+
+            Console.WriteLine(textoFormatado);
+        }
+    }
+}

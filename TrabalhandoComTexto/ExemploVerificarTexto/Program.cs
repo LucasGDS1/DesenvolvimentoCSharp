02cd59325b024500c3b9f018e95e3a9fe53ae212
lucas <lucasgbrsouza@gmail.com>
2019-11-10 17:00:52 -0300

    Classe principal

---
+++ TrabalhandoComTexto/ExemploVerificarTexto/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ExemploVerificarTexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "vamos verificar a existência da palavra: Hello";
+
+            if (texto.StartsWith("vamoss") == true)
+            {
+                Console.WriteLine("A palavra especificada também está no texto!");
+            }
+            else if (texto.EndsWith("Helloo") == true)
+            {
+                Console.WriteLine("A palavra especificada está no texto!");
+            }
+            else if (texto.Contains("existência") == true)
+            {
+                Console.WriteLine("A palavra está no texto!");
+            }
+           
+        }
+    }
+}

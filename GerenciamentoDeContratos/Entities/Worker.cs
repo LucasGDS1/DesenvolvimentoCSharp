8471993c85f6df9f4d9a0ed1132b12450acfb2f9
lucas <lucasgbrsouza@gmail.com>
2019-11-10 23:07:24 -0300

    Classe para guardar os atributos do trabalhador e gerar lista

---
+++ GerenciamentoDeContratos/Entities/Worker.cs
@@ -0,0 +1,45 @@
+﻿using GerenciamentoDeContratos.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciamentoDeContratos.Entities
+{
+    class Worker
+    {
+        public string Nome { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+        public Departament Departament { get; set; }
+
+        public Worker(string nome, WorkerLevel level, double basesalary, Departament departament )
+        {
+            Nome = nome;
+            Level = level;
+            BaseSalary = basesalary;
+            Departament = departament;
+        }
+        public void AddContract(HourContract contract)
+        {
+            Contracts.Add(contract);
+        }
+        public void RemoveContract(HourContract contract)
+        {
+            Contracts.Remove(contract);
+        }
+
+        public double Income(int year, int month)
+        {
+            double sum = BaseSalary;
+            foreach(HourContract percorre in Contracts)
+            {
+                if(percorre.Date.Year == year && percorre.Date.Month == month)
+                {
+                    sum += percorre.TotalValue();
+                }
+            }
+            return sum;
+        }
+    }
+}

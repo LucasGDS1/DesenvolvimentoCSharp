f3dff362ef4e888dd63cedeb2c2775428e8c5884
lucas <lucasgbrsouza@gmail.com>
2019-11-10 23:05:56 -0300

    Classe para guardar o nome do departamento

---
+++ GerenciamentoDeContratos/Entities/Departament.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GerenciamentoDeContratos.Entities
+{
+    class Departament
+    {
+        public string Nome { get; set; }
+
+        public Departament()
+        {
+
+        }
+        public Departament(string nome)
+        {
+            Nome = nome;
+        }
+    }
+}

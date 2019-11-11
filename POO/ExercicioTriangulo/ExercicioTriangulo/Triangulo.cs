f16871de1225dfd2bf503e1c87e9e3b73acccebb
André Vigo <andrevigo@gmail.com>
2019-11-11 10:49:05 -0300

    Classe de atributos e métodos para calcular

---
+++ POO/ExercicioTriangulo/ExercicioTriangulo/Triangulo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExercicioTriangulo
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+
+        public double calcularArea()
+        {
+            double formula = (A + B + C) / 2.0;
+            return Math.Sqrt(formula * (formula - A) * (formula - B) * (formula - C));
+        }
+    }
+}

cebfd9f998dfde5c295f679f93a1588ceeeb9612
André Vigo <andrevigo@gmail.com>
2019-11-19 13:00:35 -0300

    Classe início

---
+++ ReservaDeHotel/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using ReservaDeHotel.Entities;
+
+namespace ReservaDeHotel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                Console.Write("Room number: ");
+                int room = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkin = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkout = DateTime.Parse(Console.ReadLine());
+
+                if (checkout < checkin)
+                {
+                    Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+                }
+                else
+                {
+                    Reservation reserva = new Reservation(room, checkin, checkout);
+                    Console.WriteLine("Resernvation: " + reserva);
+
+                    Console.WriteLine();
+                    Console.WriteLine("Enter data to update the reservation: ");
+                    Console.Write("Check-in Date (dd/MM/yyyy): ");
+                    checkin = DateTime.Parse(Console.ReadLine());
+                    Console.Write("Check-out Date (dd/MM/yyyy): ");
+                    checkout = DateTime.Parse(Console.ReadLine());
+
+                    reserva.updateDates(checkin, checkout);
+                    Console.WriteLine("Reservation: " + reserva);
+                }
+            } catch(Exception e)
+            {
+                Console.WriteLine("Execeção: " + e.Message);
+            }
+        }
+    }
+}
